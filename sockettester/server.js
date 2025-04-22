9305405fd7aae787f4f3020bac56545110b9a0e5
handalab01kim <yi.kim@handalab.com>
2025-04-22 14:48:12 +0900

    socket-tester

---
+++ sockettester/server.js
@@ -0,0 +1,48 @@
+const io = require("socket.io-client")
+
+console.log("sockets on!")
+
+const ANALYSIS_IP = "192.168.1.21";
+const UI_SERVER_IP = "192.168.1.22";
+
+
+
+const timezone = (date) =>{
+    return date.toLocaleString("ko-KR", {
+        timezone: "Asia/Seoul",
+    });
+};
+
+
+
+const dio = io(`ws://${ANALYSIS_IP}:4003/output`)
+//
+dio.on("message", (data)=>{
+    console.log("\n\ndio - message", timezone(new Date()));
+    console.log(data);
+});
+
+
+
+
+const all = io(`ws://${UI_SERVER_IP}:4001/all`)
+//
+all.on("event", (data)=>{
+    console.log("\n\nall - event", timezone(new Date()));
+    console.log(data);
+});
+//
+all.on("status", (data)=>{
+    console.log("\n\nall - status", timezone(new Date()));
+    console.log(data);
+});
+
+
+
+
+const analysis = io(`ws://${UI_SERVER_IP}:4001/analysis`)
+//
+analysis.on("message", (data)=>{
+    console.log("\n\nanalysis - message", timezone(new Date()));
+    console.log(data);
+});
